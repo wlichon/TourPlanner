1cdfeb4fb9bc4a19a057d9e0542e54f5bd3146a3
zerleger <101527159+zerleger@users.noreply.github.com>
2023-02-20 22:43:55 +0100

    added main viewmodel and 3 sub viewmodels

---
+++ TourPlanner/Core/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ModernDesign.Core
+{
+    internal class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ TourPlanner/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModernDesign.Core;
+
+namespace TourPlanner.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand GeneralViewCommand { get; set; }
+
+        public RelayCommand RouteViewCommand { get; set; }
+
+        public RelayCommand OtherViewCommand { get; set; }
+
+        public GeneralViewModel GeneralVM { get; set; }
+
+        public RouteViewModel RouteVM { get; set; }
+
+        public OtherViewModel OtherVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            GeneralVM = new GeneralViewModel();
+
+            RouteVM = new RouteViewModel();
+
+            OtherVM = new OtherViewModel();
+
+            GeneralViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = GeneralVM;
+            });
+
+            RouteViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RouteVM;
+            });
+
+            OtherViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = OtherVM;
+            });
+
+            _currentView = GeneralVM;
+        }
+    }
+}
